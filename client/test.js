2d211c7e2cd1235e7686f3131e725b242432f683
Brent Noorda <brent.noorda@gmail.com>
2013-03-23 20:59:02 -0700

    separate stuff even more

--- client/test.js
+++ client/test.js
@@ -1,11 +1,11 @@
 /*jslint white:false plusplus:false browser:true nomen:false */
 /*globals Template, console */
 
-Template.hello.greeting = function () {
+Template.test.greeting = function () {
     return "Welcome to gay-or-straight.";
 };
 
-Template.hello.events({
+Template.test.events({
     'click input' : function () {
         // template data, if any, is available in 'this'
         if (typeof console !== 'undefined')
