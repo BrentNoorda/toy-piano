23e548b4656c7343397744070a209f0f12d49c77
Brent Noorda <brent.noorda@gmail.com>
2013-03-25 15:08:01 -0700

    click on name to change it

--- client/username.js
+++ client/username.js
@@ -1,5 +1,5 @@
 /*jslint white:false plusplus:false browser:true nomen:false */
-/*globals Session, Random, amplify*/
+/*globals Session, Random, amplify, tidy_spaces*/
 
 // manage the username in html5 store and in Session
 
@@ -9,7 +9,7 @@ function set_username(username)
     if ( username.length === 0 )
     {
         username = amplify.store('username');
-        if ( username === undefined )
+        if ( (username === undefined) || (username.length === 0) )
         {
             // make up a random username
             username = "? " + Math.floor(Random.fraction() * 10000.0);
