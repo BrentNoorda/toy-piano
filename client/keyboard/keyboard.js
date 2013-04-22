43b8e53b8fea92f627239c58ebec8addf3122b5d
Brent Noorda <brent.noorda@gmail.com>
2013-04-22 12:26:03 -0700

    baby steps to displaying keys

--- client/keyboard/keyboard.js
+++ client/keyboard/keyboard.js
@@ -1,9 +1,28 @@
 /*jslint white:false plusplus:false browser:true nomen:false */
 /*globals Template, console*/
 
-Template.keyboard.greeting = function () {
-    return "Welcome to toy-piano";
-};
+var keyboardWidth = -1;  // changed whenever screen size changes
+
+function change_keyboard_width()
+{
+    var new_width = $('#keyboard-case').width();
+    if ( new_width !== keyboardWidth )
+    {
+        keyboardWidth = new_width;
+        alert( keyboardWidth );
+    }
+}
+
+Template.keyboard.whiteKeys = [
+    { name: 'c' },
+    { name: 'd' },
+    { name: 'e' },
+    { name: 'f' },
+    { name: 'g' },
+    { name: 'a' },
+    { name: 'b' },
+    { name: 'c' }
+];
 
 Template.keyboard.events({
     'click input' : function () {
@@ -15,3 +34,12 @@ Template.keyboard.events({
     }
 });
 
+Template.keyboard.rendered = function() {
+     change_keyboard_width();
+}
+
+Meteor.startup = function() { // from http://stackoverflow.com/questions/14185248/rerendering-meteor-js-on-window-resize
+  $(window).resize(function(evt) {
+    change_keyboard_width();
+  });
+}
