461e021748bcc4e268d07c6cc347c5dfa3dbcc79
Brent Noorda <brent.noorda@gmail.com>
2013-04-22 11:00:34 -0700

    rename all test stuff as keyboard

--- client/keyboard/keyboard.js
+++ client/keyboard/keyboard.js
@@ -1,11 +1,11 @@
 /*jslint white:false plusplus:false browser:true nomen:false */
 /*globals Template, console*/
 
-Template.test.greeting = function () {
+Template.keyboard.greeting = function () {
     return "Welcome to toy-piano";
 };
 
-Template.test.events({
+Template.keyboard.events({
     'click input' : function () {
         // template data, if any, is available in 'this'
         if (typeof console !== 'undefined')
