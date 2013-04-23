99ccd759a9313f3992e022450e87adaaafb24419
Brent Noorda <brent.noorda@gmail.com>
2013-04-22 17:55:57 -0700

    more keypress work

--- client/keyboard/keyboard.js
+++ client/keyboard/keyboard.js
@@ -1,31 +1,32 @@
 /*jslint white:false plusplus:false browser:true nomen:false */
-/*globals $, Template, console, Session, Meteor, window*/
+/*globals $, Template, Session, Meteor, window, white_keypress:true, black_keypress:true*/
 
 var keyboardWidth = 50;  // changed whenever screen size changes
 Template.keyboard.keyboardHeight = 10;
+var press_timeout = 100;
 
 Template.keyboard.whiteKeys = [
-    { name: 'c', leftOffset:1, width:1 },
-    { name: 'd', leftOffset:2, width:1 },
-    { name: 'e', leftOffset:3, width:1 },
-    { name: 'f', leftOffset:4, width:1 },
-    { name: 'g', leftOffset:5, width:1 },
-    { name: 'a', leftOffset:6, width:1 },
-    { name: 'b', leftOffset:7, width:1 },
-    { name: 'c', leftOffset:8, width:1 }
+    { name: 'c', idx:0, leftOffset:1, width:1, timeout:null },
+    { name: 'd', idx:1, leftOffset:2, width:1, timeout:null },
+    { name: 'e', idx:2, leftOffset:3, width:1, timeout:null },
+    { name: 'f', idx:3, leftOffset:4, width:1, timeout:null },
+    { name: 'g', idx:4, leftOffset:5, width:1, timeout:null },
+    { name: 'a', idx:5, leftOffset:6, width:1, timeout:null },
+    { name: 'b', idx:6, leftOffset:7, width:1, timeout:null },
+    { name: 'c', idx:7, leftOffset:8, width:1, timeout:null }
 ];
 
 Template.keyboard.blackKeys = [
-    { name: 'c#', leftOffset:1, width:1 },
-    { name: 'd#', leftOffset:2, width:1 },
-    { name: 'f#', leftOffset:2, width:1 },
-    { name: 'g#', leftOffset:2, width:1 },
-    { name: 'a#', leftOffset:2, width:1 }
+    { name: 'c#', idx:0, leftOffset:1, width:1, timeout:null },
+    { name: 'd#', idx:1, leftOffset:2, width:1, timeout:null },
+    { name: 'f#', idx:2, leftOffset:2, width:1, timeout:null },
+    { name: 'g#', idx:3, leftOffset:2, width:1, timeout:null },
+    { name: 'a#', idx:4, leftOffset:2, width:1, timeout:null }
 ];
 
 function change_keyboard_size()
 {
-    var new_width, screen_height, new_height, white_key_width, black_key_width, i, j, keyName;
+    var new_width, screen_height, new_height, white_key_width, black_key_width, i, j, keyName, wKey, bKey;
     new_width = $('#keyboard-case').width();
     if ( new_width !== keyboardWidth )
     {
@@ -34,22 +35,29 @@ function change_keyboard_size()
         black_key_width = Math.floor(white_key_width * 2 / 3);
         for ( i = 0; i < Template.keyboard.whiteKeys.length; i++ )
         {
-            Template.keyboard.whiteKeys[i].width = white_key_width;
-            Template.keyboard.whiteKeys[i].leftOffset = (white_key_width) * i;
+            wKey = Template.keyboard.whiteKeys[i];
+            wKey.width = white_key_width;
+            wKey.leftOffset = (white_key_width) * i;
+            Meteor.clearTimeout(wKey.timeout);
+            wKey.timeout = null;
         }
         for ( j = 0; j < Template.keyboard.blackKeys.length; j++ )
         {
-            keyName = Template.keyboard.blackKeys[j].name.substring(0,1);
+            bKey = Template.keyboard.blackKeys[j];
+            keyName = bKey.name.substring(0,1);
             for ( i = 0; i < Template.keyboard.whiteKeys.length; i++ )
             {
-                if ( Template.keyboard.whiteKeys[i].name === keyName ) {
-                    Template.keyboard.blackKeys[j].width = black_key_width;
-                    Template.keyboard.blackKeys[j].leftOffset = Math.floor(
+                wKey = Template.keyboard.whiteKeys[i];
+                if ( wKey.name === keyName ) {
+                    bKey.width = black_key_width;
+                    bKey.leftOffset = Math.floor(
                         Template.keyboard.whiteKeys[i+1].leftOffset - (black_key_width/2)
                     );
                     break;
                 }
             }
+            Meteor.clearTimeout(bKey.timeout);
+            bKey.timeout = null;
         }
         Session.set('keyboardWidth',new_width);
     }
@@ -72,10 +80,29 @@ Template.keyboard.render_on_resize = function() {
     return '';
 };
 
-keypress = function(keyName) {
-    alert("press" + keyName);
-    return false;
+function key_pressed(selector,key,newClass)
+{
+    if ( key.timeout !== null )
+    {
+        Meteor.clearTimeout(key.timeout);
+    }
+    else
+    {
+        $(selector).addClass(newClass);
+    }
+    Meteor.setTimeout(function(){
+        $(selector).removeClass(newClass);
+    },press_timeout);
 }
+
+white_keypress = function(idx) {
+    key_pressed("#whitekey-" + idx,Template.keyboard.whiteKeys[idx],"white-key-pressed");
+    return false;
+};
+black_keypress = function(idx) {
+    key_pressed("#blackkey-" + idx,Template.keyboard.blackKeys[idx],"black-key-pressed");
+    return false;
+};
 
 Meteor.startup = function() { // from http://stackoverflow.com/questions/14185248/rerendering-meteor-js-on-window-resize
     $(window).resize(function(evt) {
